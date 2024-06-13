bae7a35e88f470ceeaf78522f28e988af33e09fa
italobruhhh <169067935+italobruhhh@users.noreply.github.com>
2024-06-13 10:39:41 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+  Scanner sc = new Scanner(System.in)
+
+
+   System.out.println("digite seu nome arrombado");
+    String nome = sc.next();
+
+    System.out.println("digite seu segundo nome");
+      String sobrenome = sc.next();
+
+    System.out.println("digite sua idade");
+    String idade = sc.next();
+
+    System.out.println("digite sua altura em metros (exemplo 1,60)");
+    String altura = sc.next();
+
+    System.out.println("digite sua massa em quilogramas (exemplo 69,5)");
+    String massa = sc.next();
+
+    double imc = 
+  }
+
+
+}
